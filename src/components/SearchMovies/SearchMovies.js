ff99ea00c76cbe7ac04ef3e4e0100c684fc35a21
Paul Terry <paul.terry84@gmail.com>
2019-11-18 10:31:50 +0000

    added axios for api requests

--- src/components/SearchMovies/SearchMovies.js
+++ src/components/SearchMovies/SearchMovies.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
+import axios from "axios";
+import { config } from "../../temp_config";
 
 import SearchBar from "../SearchBar";
 
 class SearchMovies extends Component {
   state = { movies: [] };
 
-  onSearchSubmit = (term) => {
-    // call tmdb api
+  onSearchSubmit = async (term) => {
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/search/movie",
+      {
+        params: {
+          api_key: config.API_KEY,
+          language: "en-US",
+          query: term,
+          page: 1,
+          include_adult: false
+        }
+      }
+    );
+
+    this.setState({ movies: response.data.results });
   };
 
   render() {

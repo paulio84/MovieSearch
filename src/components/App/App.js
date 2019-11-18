26c1a67cb3f76f836b6e0853d56bef75a2539afd
Paul Terry <paul.terry84@gmail.com>
2019-11-18 10:12:13 +0000

    adds semantic ui as the CSS styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,8 @@ import SearchMovies from "../SearchMovies";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
+      <h1>Movie Search</h1>
       <SearchMovies />
     </div>
   );
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,13 +13,14 @@ class SearchBar extends Component {
 
   render() {
     return (
-      <form onSubmit={this.onFormSubmit}>
+      <form className="ui form" onSubmit={this.onFormSubmit}>
         <input
           type="text"
           value={this.state.term}
           onChange={(event) => {
             this.setState({ term: event.target.value });
           }}
+          placeholder="Enter a movie title ..."
         />
       </form>
     );

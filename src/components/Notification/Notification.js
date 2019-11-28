3aca9d131c299bf54f28c898c9366d11505adef0
Paul Terry <stainless.crab@yahoo.com>
2019-11-28 14:32:49 +0000

    update to notification to use arrow function

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -3,21 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretUp, faTimes } from "@fortawesome/free-solid-svg-icons";
 import PropTypes from "prop-types";
 
-function Notification({ removeNotification }) {
-  return (
-    <div className="notification">
-      <span>
-        <FontAwesomeIcon icon={faCaretUp} />
+const Notification = ({ removeNotification }) => (
+  <div className="notification">
+    <span>
+      <FontAwesomeIcon icon={faCaretUp} />
+    </span>
+    <div className="notification__text">
+      <p>The search term is required.</p>
+      <span className="notification__button">
+        <FontAwesomeIcon onClick={removeNotification} icon={faTimes} />
       </span>
-      <div className="notification__text">
-        <p>The search term is required.</p>
-        <span className="notification__button">
-          <FontAwesomeIcon onClick={removeNotification} icon={faTimes} />
-        </span>
-      </div>
     </div>
-  );
-}
+  </div>
+);
 Notification.propTypes = {
   removeNotification: PropTypes.func.isRequired
 };

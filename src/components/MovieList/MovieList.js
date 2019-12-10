533efb0d7a0407a89e9b8c164e836aab763bb04a
Paul Terry <paul.terry84@gmail.com>
2019-12-10 07:43:55 +0000

    reverted MovieList back to stateless functional component

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,79 +1,75 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import dayjs from "dayjs";
 
 import LikeButton from "../LikeButton";
 
-class MovieList extends Component {
-  render() {
-    const {
-      movies,
-      favouriteMovies,
-      onAddToFavouriteMovies,
-      onRemoveFromFavouriteMovies
-    } = this.props;
+const MovieList = ({
+  movies,
+  favouriteMovies,
+  onAddToFavouriteMovies,
+  onRemoveFromFavouriteMovies
+}) =>
+  movies.length > 0 ? (
+    <section>
+      <ul>
+        {movies.map((movie) => {
+          const isMovieLiked = favouriteMovies.some(
+            (likedMovie) => likedMovie.id === movie.id
+          );
 
-    return movies.length > 0 ? (
-      <section>
-        <ul>
-          {movies.map((movie) => {
-            const isMovieLiked = favouriteMovies.some(
-              (likedMovie) => likedMovie.id === movie.id
-            );
-
-            return (
-              <li className="movie-card" key={movie.id}>
-                {movie.poster_path ? (
-                  <img
-                    src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                ) : (
-                  <img
-                    className="no-photo"
-                    src={`/assets/noPhotoFound.png`}
-                    alt={movie.title}
-                  />
-                )}
-                <div className="movie-card__content">
-                  <div className="movie-card__title">
-                    <p className="fw-bold">{movie.title}</p>
-                    <p className="fg-gray">
-                      {dayjs(movie.release_date).format("MMMM D, YYYY")}
-                    </p>
-                  </div>
-                  <div className="movie-card__infoBar">
-                    <p>
-                      <a
-                        href={`https://www.themoviedb.org/movie/${movie.id}?language=en-US`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        More info
-                      </a>
-                    </p>
-                    <LikeButton
-                      isLiked={isMovieLiked}
-                      handleLike={(isLiked) => {
-                        if (isLiked) {
-                          onAddToFavouriteMovies(movie);
-                        } else {
-                          onRemoveFromFavouriteMovies(movie);
-                        }
-                      }}
-                    />
-                  </div>
+          return (
+            <li className="movie-card" key={movie.id}>
+              {movie.poster_path ? (
+                <img
+                  src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`}
+                  alt={movie.title}
+                />
+              ) : (
+                <img
+                  className="no-photo"
+                  src={`/assets/noPhotoFound.png`}
+                  alt={movie.title}
+                />
+              )}
+              <div className="movie-card__content">
+                <div className="movie-card__title">
+                  <p className="fw-bold">{movie.title}</p>
+                  <p className="fg-gray">
+                    {dayjs(movie.release_date).format("MMMM D, YYYY")}
+                  </p>
                 </div>
-              </li>
-            );
-          })}
-        </ul>
-      </section>
-    ) : (
-      <section>No Movies Found!</section>
-    );
-  }
-}
+                <div className="movie-card__infoBar">
+                  <p>
+                    <a
+                      href={`https://www.themoviedb.org/movie/${movie.id}?language=en-US`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      More info
+                    </a>
+                  </p>
+                  <LikeButton
+                    isLiked={isMovieLiked}
+                    handleLike={(isLiked) => {
+                      if (isLiked) {
+                        onAddToFavouriteMovies(movie);
+                      } else {
+                        onRemoveFromFavouriteMovies(movie);
+                      }
+                    }}
+                  />
+                </div>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  ) : (
+    <section>No Movies Found!</section>
+  );
+
 MovieList.propTypes = {
   movies: PropTypes.arrayOf(
     PropTypes.shape({

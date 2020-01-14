95c430b0bfa860ead9e424010f78188909c9997f
Paul Terry <paul.terry84@gmail.com>
2020-01-14 08:47:30 +0000

    updated app component to use hooks, useState and useEffect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,56 +1,45 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import Footer from "../Footer";
 import SearchMovies from "../SearchMovies";
 
-class App extends Component {
-  state = { favouriteMovies: [] };
+const App = () => {
+  const [favouriteMovies, setFavouriteMovies] = useState([]);
 
-  componentDidMount() {
+  useEffect(() => {
     const favouriteMovies = JSON.parse(localStorage.getItem("favouriteMovies"));
-    if (!favouriteMovies) {
-      this.setState({ favouriteMovies: [] });
-    } else {
-      this.setState({ favouriteMovies });
-    }
-  }
+    setFavouriteMovies(favouriteMovies || []);
+  }, []);
 
-  componentDidUpdate() {
-    localStorage.setItem(
-      "favouriteMovies",
-      JSON.stringify(this.state.favouriteMovies)
-    );
-  }
+  useEffect(() => {
+    localStorage.setItem("favouriteMovies", JSON.stringify(favouriteMovies));
+  }, [favouriteMovies]);
 
-  onAddToFavouriteMovies = (movie) => {
-    this.setState({ favouriteMovies: [...this.state.favouriteMovies, movie] });
+  const onAddToFavouriteMovies = (movie) => {
+    setFavouriteMovies([...favouriteMovies, movie]);
   };
 
-  onRemoveFromFavouriteMovies = (movie) => {
-    this.setState({
-      favouriteMovies: this.state.favouriteMovies.filter(
-        (favouriteMovie) => favouriteMovie.id !== movie.id
-      )
-    });
+  const onRemoveFromFavouriteMovies = (movie) => {
+    setFavouriteMovies(
+      favouriteMovies.filter((favouriteMovie) => favouriteMovie.id !== movie.id)
+    );
   };
 
-  render() {
-    return (
-      <div className="container">
-        <header>
-          <nav className="navBar">
-            <h1>MovieSearch</h1>
-          </nav>
-        </header>
-        <SearchMovies
-          favouriteMovies={this.state.favouriteMovies}
-          onAddToFavouriteMovies={this.onAddToFavouriteMovies}
-          onRemoveFromFavouriteMovies={this.onRemoveFromFavouriteMovies}
-        />
-        <Footer />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="container">
+      <header>
+        <nav className="navBar">
+          <h1>MovieSearch</h1>
+        </nav>
+      </header>
+      <SearchMovies
+        favouriteMovies={favouriteMovies}
+        onAddToFavouriteMovies={onAddToFavouriteMovies}
+        onRemoveFromFavouriteMovies={onRemoveFromFavouriteMovies}
+      />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
